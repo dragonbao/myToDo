68c98ab7c80ada1c3d3866f18db59a216f8439fc
dragon <1563829760@qq.com>
2018-04-10 20:23:44 +0800

    2018/4/10

--- myToDo/index.js
+++ myToDo/index.js
@@ -11,12 +11,13 @@ window.onload=function(){
 			tasklist:"任务列表",
 			notask:"还没有添加任何任务哦！",
 			tasks:[
-			{task:"读一本书",ischecked:false},
-			{task:"write a small program",ischecked:false},
-			{task:"be a better man",ischecked:false},
-			{task:"catch you breath",ischecked:false}
+			{task:"读一本书",ischecked:false,isedit:false},
+			{task:"write a small program",ischecked:false,isedit:false},
+			{task:"be a better man",ischecked:false,isedit:false},
+			{task:"catch you breath",ischecked:false,isedit:false}
 			],
-			newtask:"",
+			newtask:""
+			
 			// isChecked:""
 		},
 		methods:{
@@ -25,7 +26,7 @@ window.onload=function(){
 				// console.log(this.newtask);
 				if(this.newtask!=""){
 				this.tasks.push({
-					task:this.newtask,ischecked:false
+					task:this.newtask,ischecked:false,isedit:false
 				});
 			}
 				this.newtask ="";
@@ -38,6 +39,13 @@ window.onload=function(){
 				// alert(item)
 				var index = this.tasks.indexOf(item);
 				this.tasks.splice(index,1)
+			},
+			edititem(item){
+				console.log(item);
+				item.isedit = true;
+				// var index = this.tasks.indexOf(item);
+				// var btns =document.getElemensByClassName("btns")[index];
+				// btn.style.border = "2px #33c";
 
 			}
 		}
